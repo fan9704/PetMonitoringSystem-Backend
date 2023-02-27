92a116672b8adb989430029358dee223e6b0b7b5
fan9704 <cxz123499@gmail.com>
2023-02-27 16:21:44 +0800

    Complete WebSocket Basic Tunnel

--- PetMonitoringSystemBackend/asgi.py
+++ PetMonitoringSystemBackend/asgi.py
@@ -10,7 +10,25 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from ws import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PetMonitoringSystemBackend.settings')
 
-application = get_asgi_application()
+# application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    # http请求使用这个
+    "http": get_asgi_application(),
+
+    # websocket请求使用这个
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
+
+
+
--- PetMonitoringSystemBackend/urls.py
+++ PetMonitoringSystemBackend/urls.py
@@ -34,6 +34,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('ws/',include('ws.urls')),
 
     # path('(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
