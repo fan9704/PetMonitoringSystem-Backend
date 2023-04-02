3b68b1b17b5a65d84b948ff0c0877ebb2a64ba15
fan9704 <cxz123499@gmail.com>
2023-04-02 17:20:04 +0800

    Complete FindRecordByRecordTypeAndPetName and fix its N+1 Problems

--- api/views/recordViews.py
+++ api/views/recordViews.py
@@ -1,11 +1,9 @@
-from django.contrib.auth.models import User
-from django.contrib import auth
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.models import Record, RecordType
+from api.models import Record, RecordType, Pet
 from api.serializers import RecordSerializer, RecordTypeSerializer
 
 
@@ -18,6 +16,45 @@ class RecordRUDAPIView(generics.RetrieveUpdateDestroyAPIView):
 class RecordListView(generics.ListAPIView):
     queryset = Record.objects.all()
     serializer_class = RecordSerializer
+
+
+class RecordByRecordType(APIView):
+    @swagger_auto_schema(
+        operation_id='透過RecordType搜尋所有Record',
+        operation_summary='搜尋紀錄透過記錄種類',
+        operation_description='需要帶入RecordType參數 種類名稱',
+    )
+    def get(self, request, recordType):
+        try:
+            recordTypeObject = RecordType.objects.get(type=recordType)
+            records = Record.objects.filter(type=recordTypeObject)
+            serializer = RecordSerializer(records, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as E:
+            msg = {
+                "message": "Not found Record Type"
+            }
+            return Response(msg, status.HTTP_404_NOT_FOUND)
+
+    @swagger_auto_schema(
+        operation_id='透過RecordType與Pet搜尋所有Record',
+        operation_summary='搜尋紀錄透過寵物與記錄種類',
+        operation_description='需要帶入RecordType與Pet參數 種類名稱',
+    )
+    def get(self,request,recordType,petName):
+        try:
+            # pet = Pet.objects.get(name=petName)
+            # recordTypeObject = RecordType.objects.get(type=recordType)
+            # records = Record.objects.filter(type=recordTypeObject,pet=pet)
+            records = Record.objects.select_related('pet','type').filter(pet__name=petName,type__type=recordType)
+            serializer = RecordSerializer(records, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as E:
+            msg = {
+                "message": "Not found Record Type"
+            }
+            print(E)
+            return Response(msg, status.HTTP_404_NOT_FOUND)
 
 
 # RecordTypeAPI
