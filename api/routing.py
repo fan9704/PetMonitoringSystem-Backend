aa4fae1b994c30bc0a3b9364e04a8b53d8cdb993
fan9704 <cxz123499@gmail.com>
2023-06-16 21:49:16 +0800

    Fix notify routing error

--- api/routing.py
+++ api/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from api.consumers import NotifyConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/notify/', NotifyConsumer.as_asgi()),
+    re_path(r'ws/notify/<str:username>/', NotifyConsumer.as_asgi()),
 ]
