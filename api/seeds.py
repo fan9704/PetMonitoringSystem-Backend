27a46b03c8c1de21cd2c0fa22f3dc19051ec815d
fan9704 <cxz123499@gmail.com>
2023-08-03 15:35:11 +0800

    fix: refactor variable named

--- api/seeds.py
+++ api/seeds.py
@@ -1,13 +1,20 @@
 # Command python manage.py seed
+import logging
+import os
 
 from django_seed import Seed
 from django.contrib.auth.models import User
 from api.models import RecordType
 from api.models import PetType
+from dotenv import load_dotenv
 
 
-def generatePetTypeSeedData():
-    petTypeDict = {
+load_dotenv()
+logger = logging.getLogger(__name__)
+
+
+def generate_pet_type_seed_data():
+    pet_type_dict = {
         "Cat": "This is a pet cat.",
         "Dog": "This is a pet dog.",
         "Mouse": "This is a pet mouse.",
@@ -15,19 +22,21 @@ def generatePetTypeSeedData():
     }
 
     seeder = Seed.seeder("PetType")
-    for petName, petDescription in petTypeDict.items():
+    for pet_name, pet_description in pet_type_dict.items():
         seeder.add_entity(PetType, 1, {
-            'typename': petName,
-            'description': petDescription,
+            'typename': pet_name,
+            'description': pet_description,
         })
     inserted_pks = seeder.execute()
+    logger.info(inserted_pks)
+    logger.info("Created Pet Type Seed Data")
 
 
-def generateAdminUserSeedData():
+def generate_admin_user_seed_data():
     seeder = Seed.seeder("Admin")
     seeder.add_entity(User, 1, {
         "username": "admin",
-        "password": "admin",
+        "password": os.getenv("PGADMIN_DEFAULT_PASSWORD"),
         "email": "admin@gmail.com",
         "is_superuser": True,
         "is_staff": True,
@@ -35,19 +44,25 @@ def generateAdminUserSeedData():
         "last_name": "admin",
     })
     inserted_pks = seeder.execute()
+    logger.info(inserted_pks)
+    logger.info("Created Admin User Seed Data")
 
 
-def generateRecordTypeSeedData():
+def generate_record_type_seed_data():
     seeder = Seed.seeder("RecordType")
-    recordTypeList = ["weight", "water", "humidity", "temperature", "food"]
-    for i in recordTypeList:
+    record_type_list = ["weight", "water", "humidity", "temperature", "food"]
+    for i in record_type_list:
         seeder.add_entity(RecordType, 1, {
             "type": i
         })
     inserted_pks = seeder.execute()
+    logger.info(inserted_pks)
+    logger.info("Created Record Type Seed Data")
 
 
 if __name__ == '__main__':
-    generatePetTypeSeedData()
-    generateAdminUserSeedData()
-    generateRecordTypeSeedData()
+    logger.info("Start Init Seed Data")
+    generate_record_type_seed_data()
+    generate_admin_user_seed_data()
+    generate_pet_type_seed_data()
+    logger.info("Complete Created All Seed Data")
