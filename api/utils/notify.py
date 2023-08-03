64d83329b685bd452b2481fc0f33c5b34cb9fd1d
fan9704 <cxz123499@gmail.com>
2023-08-03 15:36:17 +0800

    fix: refactor firebase config

--- api/utils/notify.py
+++ api/utils/notify.py
@@ -11,23 +11,23 @@ from api.models import FcmToken
 
 logger = logging.getLogger(__name__)
 
-cred = credentials.Certificate(settings.FIREBASE_CONFIG_PATH)
+cred = credentials.Certificate(settings.FIREBASE_CONFIG)
 firebase_admin.initialize_app(cred)
 
 
-def getUserToken(userId: int = None):
+def get_user_token(user_id: int = None):
     try:
-        if userId is None:
+        if user_id is None:
             logger.warning("User ID is None")
         else:
-            token = FcmToken.objects.select_related("uid").get(id=userId).token
+            token = FcmToken.objects.select_related("uid").get(id=user_id).token
             return token
     except User.DoesNotExist:
         logger.warning("User.DoesNotExist")
 
 
-def notify(title: str, body: str, userId: int = None):
-    token = getUserToken(userId=userId)
+def notify(title: str, body: str, user_id: int = None):
+    token = get_user_token(user_id=user_id)
     logger.info(token)
     message = messaging.Message(
         notification=messaging.Notification(
@@ -37,14 +37,13 @@ def notify(title: str, body: str, userId: int = None):
         data={
             "petname": "Cat1"
         },
-        # topic="flutter_notification",
         token=token
     )
     response = messaging.send(message)
-    logger.info(f'Successfully sent message User:{userId} Response:{response}')
+    logger.info(f'Successfully sent message User:{user_id} Response:{response}')
 
 
-def httpNotify(title: str, body: str):
+def http_notify(title: str, body: str):
     FCM_API_KEY = "ya29.a0AbVbY6MSnYsqSopTQnm-TqfK0ixgIDOo1SdVxMcPWeKylVOET28z5DpWAi0zijymxs-OBzu5xGrD3p0qY1Ks1lN2P_Sxo9yaWG3aDNIlKsU9qc35ZxxvDSBR6vdlNApEBoNHo51Kf59jsQl2Nk5Q82-N3fXSaCgYKAfwSARMSFQFWKvPlpuxoXdVqHg-r3pEBsp-N3A0163"
     FCM_TOKEN = os.getenv("FCM_DEVICE_TOKEN", None)
 
