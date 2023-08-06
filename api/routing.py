ccfe5cd1c4aba20f44854216d09d5e5ecb96a8bf
fan9704 <cxz123499@gmail.com>
2023-08-06 15:45:59 +0800

    fix: Websocket Routing Name defined

--- api/routing.py
+++ api/routing.py
@@ -4,6 +4,6 @@ from api.consumers.notifyConsumer import NotifyConsumer
 from api.consumers.chatConsumer import ChatConsumer
 
 websocket_urlpatterns = [
-    path('ws/notify/<str:username>/', NotifyConsumer.as_asgi()),
-    path('ws/chat/<str:username>/', ChatConsumer.as_asgi()),
+    path('ws/notify/<str:username>/', NotifyConsumer.as_asgi(), name='notify-consumer'),
+    path('ws/chat/<str:username>/', ChatConsumer.as_asgi(), name='chat-consumer'),
 ]
