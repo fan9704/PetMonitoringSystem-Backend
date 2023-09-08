84b6e8dd7361e02cb1e160f5cbd025da8d57f8c2
fan9704 <cxz123499@gmail.com>
2023-09-08 22:57:22 +0800

    refactor: rename method with linter regression

--- api/management/commands/callback/recordCallBack.py
+++ api/management/commands/callback/recordCallBack.py
@@ -1,53 +1,50 @@
 import json
-import logging
 from api import models
 from api.management.commands.logger.commandLogger import CommandLogger
 
-# logger = logging.getLogger("Record Listener")
-logger = CommandLogger("Record Listener").getLogger()
+logger = CommandLogger(__name__).get_logger()
 
 
-def distanceCallBack(topic: str, body: str, ch=None, method=None, properties=None):
+def distance_callback(topic: str, body: str, ch=None, method=None, properties=None):
     data = json.loads(body)
     logger.info("[Distance] Received " + str(data["Distance"]))
-    # ch.basic_ack(delivery_tag=method.delivery_tag)
+    logger.debug(f'Topic:{topic} ch:{ch} properties:{properties} method:{method}')
 
 
-def temperatureAndHumidityCallBack(topic: str, body: str, ch=None, method=None, properties=None):
+def temperature_humidity_callback(topic: str, body: str, ch=None, method=None, properties=None):
     data = json.loads(body)
     logger.info("[Temperature] Received " + str(data["Temperature"]))
     logger.info("[Humidity] Received " + str(data["Humidity"]))
-    # ch.basic_ack(delivery_tag=method.delivery_tag)
-    # machine = models.Machine.objects.get(name=data["machineId"])
+    logger.debug(f'Topic:{topic} ch:{ch} properties:{properties} method:{method}')
     try:
         machine = models.Machine.objects.get(name=topic.split("/")[1])
     except models.Machine.DoesNotExist:
         machine = models.Machine.objects.create(
             name=topic.split("/")[1]
         )
-    temperatureRecordType = models.RecordType.objects.get(type="temperature")
-    humidityRecordType = models.RecordType.objects.get(type="humidity")
+    temperature_record_type = models.RecordType.objects.get(type="temperature")
+    humidity_record_type = models.RecordType.objects.get(type="humidity")
     temperature = models.Record.objects.create(
         pet=machine.pet,
-        type=temperatureRecordType,
+        type=temperature_record_type,
         data=data["Temperature"],
     )
     humidity = models.Record.objects.create(
         pet=machine.pet,
-        type=humidityRecordType,
+        type=humidity_record_type,
         data=data["Humidity"],
     )
     temperature.save()
     humidity.save()
 
 
-def weightCallBack(topic: str, body: str, ch=None, method=None, properties=None):
+def weight_callback(topic: str, body: str, ch=None, method=None, properties=None):
     data = json.loads(body)
     logger.info("[Weight] Received " + str(data["Weight"]))
-    # ch.basic_ack(delivery_tag=method.delivery_tag)
+    logger.debug(f'Topic:{topic} ch:{ch} properties:{properties} method:{method}')
 
 
-def waterCallBack(topic: str, body: str, ch=None, method=None, properties=None):
+def water_callback(topic: str, body: str, ch=None, method=None, properties=None):
     data = json.loads(body)
     logger.info("[Water] Received " + str(data["Water"]))
-    # ch.basic_ack(delivery_tag=method.delivery_tag)
+    logger.debug(f'Topic:{topic} ch:{ch} properties:{properties} method:{method}')
